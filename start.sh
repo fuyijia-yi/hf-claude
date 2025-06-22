465fde4b0c38587230b3ad2682af9bf803cc63d3
fuyijia-yi <142585822+fuyijia-yi@users.noreply.github.com>
2025-06-22 19:25:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
+echo "Start Successfully"
 #Open Nginx and OA
-pm2 start nginx --name "nginx" --interpreter none -- -g "daemon off;"
-# 启动 one-api
-pm2 start /one-api --name "OA" --interpreter none
-# 保持 pm2 运行
-pm2 logs
+nginx &
+/one-api
 
 
